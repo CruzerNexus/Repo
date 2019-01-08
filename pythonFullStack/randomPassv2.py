605b8684ba038d329fb588ab609735de0554b2b9
Greg Cruz <therealcruzer@outlook.com>
2019-01-08 13:33:21 -0800

    update

--- pythonFullStack/randomPassv2.py
+++ pythonFullStack/randomPassv2.py
@@ -3,9 +3,15 @@ import random
 entry = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 count = 0
 password = ("Your password is: ")
-n = input("How long would you like your password to be? ")
-n = int(n)
+while True:
+    n = input("How long would you like your password to be (6-20 characters)? ")
+    n = int(n)
+    if 5 < n < 21:
+        break
+print("Generating password...")
 while count < n:
+    print("...")
     password += random.choice(entry)
     count += 1
+
 print(password)
