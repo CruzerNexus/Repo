8b6cf6fc368438335c8cec763bc8470e1514d552
Greg Cruz <therealcruzer@outlook.com>
2019-01-08 16:30:18 -0800

    cents 1 and 2 entered

---
+++ pythonFullStack/makeChangeV2.py
@@ -0,0 +1,25 @@
+# makeChange.py
+quarters = 0
+dimes = 0
+nickels = 0
+pennies = 0
+while True:
+    cash = input("Please enter amount in dollars (x.xx): $")
+    cash = float(cash)
+    cash = cash * 100
+    cash = int(cash)
+    if type(cash) is int:
+        break
+dollars = cash // 100
+cash = cash % 100
+if cash != 0:
+    quarters = cash // 25
+    cash = cash % 25
+    if cash != 0:
+        dimes = cash // 10
+        cash = cash % 10
+        if cash != 0:
+            nickels = cash // 5
+            pennies = cash % 5
+
+print(f"There are {dollars} dollars, {quarters} quarters, {dimes} dimes, {nickels} nickels, and {pennies} pennies.")
